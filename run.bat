815c08a4aa28d230aa8565510136680735eb6a5e
DEEPAK KONA <deepak.kona815@gmail.com>
2024-02-09 20:26:42 +0530

    Added Rerun package

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 pytest -v -s -m "sanity or regression" --browser chrome --html="Reports\report.html" -n=2
-rem pytest -v -s -m "sanity or regression" --browser chrome --html="Reports\report.html" -n=2 --alluredir="C:\Users\SAI-PC\Desktop\pythonProject001/Reports"
+rem pytest -v -s -m "sanity or regression" --browser chrome --html="Reports\report.html" --reruns 2 -n=2 --alluredir="C:\Users\SAI-PC\Desktop\pythonProject001/Reports"
